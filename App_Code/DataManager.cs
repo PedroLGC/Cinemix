b1b2894660b563ba4b102285fb022c89279631cd
KaiMilo <62195892+KaiMilo@users.noreply.github.com>
2022-03-31 15:33:29 -0600

    Update DataManager.cs

--- App_Code/DataManager.cs
+++ App_Code/DataManager.cs
@@ -130,7 +130,7 @@ namespace BaseDatosCinemix
             {
                 var horarios = from c in bd.Horario
                                where c.IdCinePelicula == idPelicula
-                               select c;
+                               select c.horario1;
 
                 return horarios.ToList();
             }
